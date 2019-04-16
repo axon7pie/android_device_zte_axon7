37802b785653765bd0c6fe1fa55b035987becd59
laci200270 <laci200270@gmail.com>
2019-04-16 19:32:36 +0200

    axon7: fix doze building error
    Change-Id: I77dc2db61ffe5124c043dd48ed0e96914ae04ab4

--- doze/Android.mk
+++ doze/Android.mk
@@ -2,14 +2,12 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_SDK_VERSION := system_current
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := ZteDoze
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
-
-LOCAL_SDK_VERSION := system_current
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_USE_AAPT2 := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \

e44518b34fd69dc403155096dcf994afb69bc5d2
laci200270 <laci200270@gmail.com>
2019-04-19 22:00:29 +0000

    axon7: update gps source from nitrogen os's oneplus3 repo
    Change-Id: I249f2b976ad30760a5327c1d1cec19a4c2af1314

--- gps/core/Android.mk
+++ gps/core/Android.mk
@@ -1,13 +1,17 @@
+ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
+ifneq ($(BUILD_TINY_ANDROID),true)
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libloc_core
 LOCAL_VENDOR_MODULE := true
-LOCAL_MODULE_OWNER := qti
 LOCAL_MODULE_TAGS := optional
 
-ifeq ($(BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET),true)
+ifeq ($(TARGET_DEVICE),apq8026_lw)
+LOCAL_CFLAGS += -DPDK_FEATURE_SET
+else ifeq ($(BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET),true)
 LOCAL_CFLAGS += -DPDK_FEATURE_SET
 endif
 
@@ -17,8 +21,7 @@ LOCAL_SHARED_LIBRARIES := \
     libcutils \
     libgps.utils \
     libdl \
-    liblog \
-    libloc_pla
+    liblog
 
 LOCAL_SRC_FILES += \
     LocApiBase.cpp \
@@ -40,6 +43,7 @@ LOCAL_C_INCLUDES:= \
     $(LOCAL_PATH)/observer \
 
 LOCAL_HEADER_LIBRARIES := \
+    libutils_headers \
     libgps.utils_headers \
     libloc_pla_headers \
     liblocation_api_headers
@@ -56,3 +60,6 @@ LOCAL_EXPORT_C_INCLUDE_DIRS := \
     $(LOCAL_PATH)/data-items/common \
     $(LOCAL_PATH)/observer
 include $(BUILD_HEADER_LIBRARY)
+
+endif # not BUILD_TINY_ANDROID
+endif # BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE

70c4b95e12dbaa507774aa874e94bf762cc30e69
laci200270 <laci200270@gmail.com>
2019-04-24 21:17:11 +0000

    axon7: update gps from oneplus3
    Change-Id: I8b89f7c955417c62b888d8a5e156603223b442d4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -251,6 +251,7 @@ TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_NTFS_3G := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_NO_USB_STORAGE := true
+BOARD_DISABLE_BLOCK_BASED_OTA := true
 
 # inherit from the proprietary version
 -include vendor/zte/axon7/BoardConfigVendor.mk
